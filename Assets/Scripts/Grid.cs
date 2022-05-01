fa2504e282f23e6bf257c93c2b72890a6bc3f133
ottosales <otaviocsales@gmail.com>
2022-05-01 02:12:18 -0300

    adding unity system, units walk to click point

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour {
-	public bool onlyDisplayPathGizmos;
-	public Transform player;
+	public bool displayGridGizmos;
 	public LayerMask unwalkableMask;
 	public Vector2 gridWorldSize;
 	public float nodeRadius;
@@ -13,7 +12,7 @@ public class Grid : MonoBehaviour {
 	float nodeDiameter;
 	int gridSizeX, gridSizeY;
 
-	void Start() {
+	void Awake() {
 		nodeDiameter = nodeRadius * 2;
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
@@ -39,7 +38,7 @@ public class Grid : MonoBehaviour {
 		}
 	}
 
-	public List<Node> GetNeighbouts(Node node) {
+	public List<Node> GetNeighbours(Node node) {
 		List<Node> neighbours = new List<Node>();
 		for (int x = -1; x <= 1; x++) {
 			for (int y = -1; y <= 1; y++) {
@@ -70,29 +69,12 @@ public class Grid : MonoBehaviour {
 		return grid[x, y];
 	}
 
-	public List<Node> path;
 	void OnDrawGizmos() {
 		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
-
-		if (onlyDisplayPathGizmos) {
-			if (path != null) {
-				foreach (Node n in path) {
-					Gizmos.color = Color.black;
-					Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
-				}
-			}
-		} else {
-			if (grid != null) {
-				Node playerNode = NodeFromWorldPoint(player.position);
-				foreach (Node n in grid) {
-					Gizmos.color = (n.walkable) ? Color.white : Color.red;
-					if (path != null) {
-						if (path.Contains(n)) {
-							Gizmos.color = Color.black;
-						}
-					}
-					Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
-				}
+		if (grid != null && displayGridGizmos) {
+			foreach (Node n in grid) {
+				Gizmos.color = (n.walkable) ? Color.white : Color.red;
+				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
 			}
 		}
 	}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+
+	public Transform target;
+	float speed = 25;
+	Vector3[] path;
+	int targetIndex;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Fire2")) {
+			Vector3 mousePosition = Input.mousePosition;
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				Vector3 targetPoint = hit.point;
+				PathRequestManager.RequestPath(transform.position, targetPoint + new Vector3(0, 3.6f, 0), OnPathFound);
+			}
+		}
+	}
+
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
+		if (pathSuccessful) {
+			path = newPath;
+			targetIndex = 0;
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
+
+	IEnumerator FollowPath() {
+		Vector3 currentWaypoint = path[0];
+		while (true) {
+			if (transform.position == currentWaypoint) {
+				targetIndex++;
+				if (targetIndex >= path.Length) {
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+			}
+			transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+			yield return null;
+
+		}
+	}
+
+	public void OnDrawGizmos() {
+		if (path != null) {
+			for (int i = targetIndex; i < path.Length; i++) {
+				Gizmos.color = Color.black;
+				Gizmos.DrawCube(path[i], Vector3.one);
+
+				if (i == targetIndex) {
+					Gizmos.DrawLine(transform.position, path[i]);
+				} else {
+					Gizmos.DrawLine(path[i - 1], path[i]);
+				}
+			}
+		}
+	}
+}
